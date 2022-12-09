604529731de6e2a68a3345af2528db6ff0d62e3e
Christian Rivera <108821784+CRivera975@users.noreply.github.com>
2022-12-08 19:57:18 -0800

    Update AssassinNode.java

--- AssassinNode.java
+++ AssassinNode.java
@@ -1,1 +1,13 @@
-
+public class AssassinNode {
+    private String player;
+    private String killer;
+    private AssassinNode next;
+    public AssassinNode(String player) {
+        this(player, null);
+    }
+    public AssassinNode(String player, AssassinNode next) {
+        this.player = player;
+        this.killer = null;
+        this.next = next;
+    }
+}

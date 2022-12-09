7585304106ef00f12320c7cf59e4c3d357f89341
Christian Rivera <108821784+CRivera975@users.noreply.github.com>
2022-12-08 20:01:10 -0800

    Update AssassinNode.java

--- AssassinNode.java
+++ AssassinNode.java
@@ -1,11 +1,11 @@
 public class AssassinNode {
-    private String player;
-    private String killer;
-    private AssassinNode next;
-    public AssassinNode(String player) {
+    private String player; //Name of player
+    private String killer; //Nameo of person who killed the player
+    private AssassinNode next; //Points to the next node
+    public AssassinNode(String player) { //Node with  player name and a null pointer
         this(player, null);
     }
-    public AssassinNode(String player, AssassinNode next) {
+    public AssassinNode(String player, AssassinNode next) { //Node with player name, points to next node
         this.player = player;
         this.killer = null;
         this.next = next;

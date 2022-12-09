1cff481482f14c50c21436a362a6bef31a20907a
Alexander <one4theghost@gmail.com>
2022-12-09 15:55:55 -0800

    Changed Array to List

--- AssassinMain.java
+++ AssassinMain.java
@@ -5,22 +5,17 @@ public class AssassinMain{
     //  Imports names from a namelist
     //  Imports into a stack
     //  Converts stack into an array
-    public static String[] getNameList() throws IOException{
+    public static List<String> getNameList() throws IOException{
 
         Scanner scanner = new Scanner(new FileReader ("names.txt"));
+        List<String> nameList = new ArrayList<String>();
 
-        int i = 0;
-        Stack<String> st = new Stack<String>();
         while (scanner.hasNextLine()){
-            st.push(scanner.nextLine());
-            i++;
+            String name = scanner.nextLine();
+            nameList.add(name);
         }
-
         scanner.close();
-
-        String[] nameArray = new String[i];
-        st.toArray(nameArray);
-        return nameArray;
+        return nameList;
 
 
     }
@@ -42,16 +37,18 @@ public class AssassinMain{
         System.out.println("Current graveyard");
         printGraveyard();
     }
-
+    
     //  TODO DELETE THIS FUNCTION BEFORE PUBLISHING
-    public static void DEBUGPRINTARRAY(String[] nameArray){
-        for (int i = 0; i < nameArray.length; i++){
-            System.out.println("Element " + i + " = " + nameArray[i]);
+    public static void DEBUGPRINTARRAY(List<String> nameList){
+        for (int i = 0; i < nameList.size(); i++){
+            System.out.println("Element " + i + " = " + nameList.get(i));
         }
     }
     public static void main (String args[]) throws IOException {
-        String[] nameArray = getNameList();
-        DEBUGPRINTARRAY(nameArray);
+        List<String> nameList = getNameList();
+        DEBUGPRINTARRAY(nameList);
+        
+        AssassinManager AssassinManager = new AssassinManager(nameList);
         
         //
         //  AssassinManager goes here
